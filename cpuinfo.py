8e66b7205ba71d139443c8b79fac9f3fb818a6ee
Jon Mentzell <jmentzell@redhat.com>
2023-04-13 22:23:25 -0400

    testing python

---
+++ cpuinfo.py
@@ -0,0 +1,7 @@
+print("Processors: ")
+with open("/proc/cpuinfo", "r")  as f:
+    info = f.readlines()
+
+cpuinfo = [x.strip().split(":")[1] for x in info if "model name"  in x]
+for index, item in enumerate(cpuinfo):
+    print("    " + str(index) + ": " + item)
